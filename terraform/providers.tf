3bb4902c156dbaa3a63c04a5b5f9e11d09431406
Nghia Pham <nghia.t.pham@outlook.com>
2025-12-08 17:33:27 -0500

    fix: Upgrade AzureRM provider to v4.0 and remove invalid config block

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0" # Upgraded to support Flex Consumption
     }
     random = {
       source  = "hashicorp/random"
@@ -16,4 +16,5 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "712c5cd3-626a-449d-93bc-d2bd155dc864" # Explicitly set subscription if needed, or rely on env vars
 }
