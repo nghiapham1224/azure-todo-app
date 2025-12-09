eabbc86cede43588998e1e242cfbc75ebbb64006
Nghia Pham <nghia.t.pham@outlook.com>
2025-12-08 23:37:06 -0500

    chore: Update infrastructure and variable configurations

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,10 +16,8 @@ variable "location" {
   default     = "eastus2"
 }
 
-# Removed sql_admin_password variable as it's now fetched from Key Vault
-
 variable "aad_admin_object_id" {
   type        = string
   description = "Object ID of the Azure AD Admin for SQL"
-  default     = "476e07a0-1245-4c8d-81b3-5abe17f2c0e6" # Your ID
+  default     = "476e07a0-1245-4c8d-81b3-5abe17f2c0e6" # My user ID
 }
